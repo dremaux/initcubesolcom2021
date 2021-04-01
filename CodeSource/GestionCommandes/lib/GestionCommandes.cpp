d96e27101575522ef434e3d9df3b3c62f608acbc
ldremaux <ldremaux@btslivh.eu>
2021-04-01 13:36:00 +0200

    rajout mongocxx.h

--- CodeSource/GestionCommandes/lib/GestionCommandes.cpp
+++ CodeSource/GestionCommandes/lib/GestionCommandes.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
 #include <json.hpp>
 #include "../include/GestionCommandes.h"
+#include "../include/mongocxx.h"
 
-using namespace std;
 
 // 
 void GestionCommandes::transmettreDernieresCommandes(){
@@ -12,5 +11,18 @@ void GestionCommandes::transmettreDernieresCommandes(){
 
 // récupère les trames de la BDD 
 void GestionCommandes::DernieresCommandes(int){
-    cout << "" << endl;
+    
+    mongocxx::instance instance{};
+    mongocxx::client client{mongocxx::uri{}};
+
+    mongocxx::database db = client["initcube"];
+    mongocxx::collection coll = db["commande"];
+
+
+    // affiche tout les éléments de la collection
+    mongocxx::cursor cursor = coll.find({});
+    for(auto doc : cursor) {
+    cout << bsoncxx::to_json(doc) << endl;
+}
+
 }
