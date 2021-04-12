c8890f1c3bc096c8fc2b77237e196645297506c9
alopes1 <alopes@btslivh.eu>
2021-04-12 18:53:16 +0200

    classe measure avec matrice et valeur simple testé et fonctionnel

--- CodeSource/InitCubeServeur/include/Measure.hpp
+++ CodeSource/InitCubeServeur/include/Measure.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "Lib.h"
 #include "Matrice.hpp"
+#include "Simple.hpp"
 
 using namespace std;
 using json = nlohmann::json;
@@ -16,7 +17,9 @@ public:
     ~Measure();
 
 private:
+    string reponse = "";
     string trame = "";
     string type = "";
     Matrice* matrice = new Matrice();
+    Simple* simple = new Simple();
 };
--- CodeSource/InitCubeServeur/src/Measure.cpp
+++ CodeSource/InitCubeServeur/src/Measure.cpp
@@ -24,7 +24,44 @@ void Measure::setTrame(string trame){
 }
 
 string Measure::genererTrame(){
-    
+    json instrument;
+    instrument =R"({
+        "INITCUBE": {
+            "DESCRIPTION": {},
+            "TRAME": {},
+            "ETAT": {},
+            "INSTRUMENT": [
+                {"DESCRIPTION":{},"ETAT":{},"TYPEMEASURE":{"NOM":"temperature","ID":"TC","TYPE":"simple","UNITE": "°C"}},
+                {"DESCRIPTION":{},"ETAT":{},"TYPEMEASURE":{"NOM":"matrice","ID":"PIX","TYPE":"matrice"}}
+            ]
+        }
+    })"_json;
+    if(instrument.find("INITCUBE") != instrument.end() && instrument["INITCUBE"].find("INSTRUMENT") != instrument["INITCUBE"].end()){
+        for(int i = 0;i<instrument["INITCUBE"]["INSTRUMENT"].size();i++){
+            string id = instrument["INITCUBE"]["INSTRUMENT"][i]["TYPEMEASURE"]["ID"];//si on ne stock pas le resultat et que on le test directement il retourne des ""
+            if(type == id){
+                string typeMesure = instrument["INITCUBE"]["INSTRUMENT"][i]["TYPEMEASURE"]["TYPE"];
+                if(typeMesure == "simple"){
+                    simple->extraireDonner((char*)trame.c_str(),type.length());
+                    reponse = simple->genererTrame(instrument["INITCUBE"]["INSTRUMENT"][i]["TYPEMEASURE"]["NOM"],instrument["INITCUBE"]["INSTRUMENT"][i]["TYPEMEASURE"]["UNITE"]);
+                    return reponse;
+                }
+                if(typeMesure == "matrice"){
+                    matrice->extraireDonnee((char*)trame.c_str(),type.length());
+                    reponse = matrice->genereTrame();
+                    return reponse;
+                }
+                if(typeMesure == "image"){
+                    cout<<"appel methode image"<<endl;
+                }
+            }
+        }
+    }
+    else{
+        cout<<"trame non conforme"<<endl;
+    }
+    return "";
+
 }
 
 Measure::~Measure() {
