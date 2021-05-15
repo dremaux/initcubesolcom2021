d0647c916d73afeebd8ed5fa2f1ee13c41277619
ocommeng <olivier-alain.commenge@ac-toulouse.fr>
2021-05-15 18:56:08 +0200

    Correction du code de la classe GestionCommandes

--- CodeSource/GestionCommandes/src/gestionCommandes.cpp
+++ CodeSource/GestionCommandes/src/gestionCommandes.cpp
@@ -28,42 +28,63 @@ using namespace std;
 
 GestionCommandes::GestionCommandes()
 {
-  mongocxx::instance instance{};
-  mongocxx::client client{mongocxx::uri{}};
-
-  mongocxx::database db = client["initcube"];
-  mongocxx::collection coll = db["commande"];
+  //choix de la base de donneés (initcube) et de la collection (commande)
+  db = client["initcube"];
+  coll = db["commande"];
 }
 
-int GestionCommandes::getDerniereCommandes()
+//getDernieresCommandes() prend en paramètre le nombre de commandes souhaitées
+int GestionCommandes::getDernieresCommandes(int nombre)
 {
+  //on réinitialise le tableau des dernières commandes
+  this->commandes.clear();
 
+  //on prépare le filtre de recherche
   mongocxx::options::find opts;
-  opts.sort(make_document(kvp("CMD.DATE", -1))).limit(3);
+  opts.sort(make_document(kvp("CMD.DATE", -1))).limit(nombre);
 
+  //on effectue la requête et on stocke le résultat dans cursor
   auto cursor = db["commande"].find({}, opts);
-  for (auto &&doc : cursor)
+  //int nombreCommandes = cursor.size();
+
+  //initialisation de la variable permettant de compter le nombre de commandes reçues
+  int nombreCommandes = 0;
+  //on traite le résultat de la requête
+  for (auto doc : cursor)
   {
-    cout << to_json(doc) << endl;
+    //pour chaque nouvelle commande trouvée, on l'ajoute au tableau des commandes (push_back())
+    this->commandes.push_back(to_json(doc));
+    //On incrémente le nombre de commandes
+    nombreCommandes++;
   }
   return nombreCommandes;
 }
 
-int GestionCommandes::rechercheCommandes()
+int GestionCommandes::rechercherCommandesParDate(std::string date)
 {
-
+  this->commandes.clear();
   bsoncxx::builder::stream::document document{};
-  document << "CMD.DATE" << 3; // on précise ce que l'on cherche
+  //On recherche les enregistrements possédant la date passée en paramètre de la méthode (utilisation des expressions régulières)
+  document << "CMD.DATE" << bsoncxx::types::b_regex{"^"+date}; 
   auto cursor = coll.find(document.view());
-
-  for (auto &&doc : cursor)
-  {
-    cout << to_json(doc) << endl;
+  //autre façon de faire la requête
+  //auto cursor = coll.find(make_document(kvp("CMD.DATE", bsoncxx::types::b_regex{"^"+date})));
+  int nombreCommandes = 0;
+  for (auto doc : cursor){
+    //pour chaque nouvelle commande trouvée, on l'ajoute au tableau des commandes (push_back())
+    this->commandes.push_back(to_json(doc));
+    //On incrémente le nombre de commandes
+    nombreCommandes++;
   }
-  return DerniereCommandes;
+  return nombreCommandes;
 }
 
-int GestionCommandes::tDCommandes()
+int GestionCommandes::transmettreCommandes()
 {
-  return 0;
+  int nbreCommandesTransmises = 0;
+  for (int i = 0;i<commandes.size();i++) {
+    cout << commandes[i] <<endl;
+    nbreCommandesTransmises++;
+  }
+  return nbreCommandesTransmises;
 }
--- CodeSource/GestionCommandes/src/main.cpp
+++ CodeSource/GestionCommandes/src/main.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <string>
 #include "gCommandes.h"
 
 using namespace std;
 
 int main(){
+    
+    //création de l'instance de Mongocxx qui doit être unique pour chaque programme
+    mongocxx::instance instance{};
 
     GestionCommandes * gCommandes = new GestionCommandes();
 
-    gCommandes->getDerniereCommandes();
+    //Récupération et transmission de toutes les dernières commandes
+    cout << "nombre de commandes reçues : " << gCommandes->getDernieresCommandes() << endl;
+    gCommandes->transmettreCommandes();
+
+    //Récupération et transmission des 5 dernières commandes
+    cout << "nombre de commandes reçues : " << gCommandes->getDernieresCommandes(5) << endl;
+    gCommandes->transmettreCommandes();
+
+    //Recherche par date
+    cout << "Recherche par la date :" << endl;
+    gCommandes->rechercherCommandesParDate("2021-05-14");
+    gCommandes->transmettreCommandes();
 
     return 0;    
 }
